1ba6f0dca9ee288b848d2a3ca3da19e53087e4ad
dengyanbo <dengyanbodyb@gmail.com>
2018-07-07 20:21:19 -0500

    implemented writecurblock()

--- source/storage_mgr.c
+++ source/storage_mgr.c
@@ -47,7 +47,22 @@ RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
     return RC_OK;
 }
 
-RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){return 0;}
+RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    if(fHandle->mgmtInfo == NULL) {
+        printError("Write Error! File header not found!\n");
+        return RC_WRITE_FAILED;
+    }
+    if(fHandle->totalNumPages < fHandle->curPagePos) {
+        printError("Write Error! Page number not found!\n");
+        return RC_WRITE_FAILED;
+    }
+    fseek(fHandle->mgmtInfo, fHandle->curPagePos * PAGE_SIZE, SEEK_SET);
+    if(fwrite(memPage, PAGE_SIZE, 1, fHandle->mgmtInfo) != 1){
+        printError("Write Error! Cannot write page to file!\n");
+        return RC_WRITE_FAILED;
+    }
+    return RC_OK;
+}
 RC appendEmptyBlock (SM_FileHandle *fHandle){return 0;}
 RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){return 0;}
 //*****************************************************************************
