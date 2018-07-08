c5eddab7b62c45c24198b7cac724fe7df9896fb2
dengyanbo <dengyanbodyb@gmail.com>
2018-07-07 20:13:33 -0500

    implemented writeblock()

--- source/storage_mgr.c
+++ source/storage_mgr.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "storage_mgr.h"
 
@@ -29,7 +30,23 @@ RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){return 0;}
 
 /* writing blocks to a page file */
 //************************************YD***************************************
-RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){return 0;}
+RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+    if(fHandle->mgmtInfo == NULL) {
+        printError("Write Error! File header not found!\n");
+        return RC_WRITE_FAILED;
+    }
+    if(fHandle->totalNumPages < pageNum) {
+        printError("Write Error! Page number not found!\n");
+        return RC_WRITE_FAILED;
+    }
+    fseek(fHandle->mgmtInfo, pageNum * PAGE_SIZE, SEEK_SET);
+    if(fwrite(memPage, PAGE_SIZE, 1, fHandle->mgmtInfo) != 1){
+        printError("Write Error! Cannot write page to file!\n");
+        return RC_WRITE_FAILED;
+    }
+    return RC_OK;
+}
+
 RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){return 0;}
 RC appendEmptyBlock (SM_FileHandle *fHandle){return 0;}
 RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){return 0;}
