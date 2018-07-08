31fca81f2907c92ced73480345f13401951f0de7
dengyanbo <dengyanbodyb@gmail.com>
2018-07-08 01:20:03 -0500

    implemented appendEmptyBlock() and fixed several bugs

--- source/storage_mgr.c
+++ source/storage_mgr.c
@@ -33,7 +33,7 @@ RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){return 0;}
 RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
     if(fHandle->mgmtInfo == NULL) {
         printError("Write Error! File header not found!\n");
-        return RC_WRITE_FAILED;
+        return RC_FILE_NOT_FOUND;
     }
     if(fHandle->totalNumPages < pageNum) {
         printError("Write Error! Page number not found!\n");
@@ -48,21 +48,43 @@ RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
 }
 
 RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage) {
+    //int pos = getBlockPos(fHandle);
     if(fHandle->mgmtInfo == NULL) {
         printError("Write Error! File header not found!\n");
-        return RC_WRITE_FAILED;
+        return RC_FILE_NOT_FOUND;
     }
     if(fHandle->totalNumPages < fHandle->curPagePos) {
         printError("Write Error! Page number not found!\n");
         return RC_WRITE_FAILED;
     }
-    fseek(fHandle->mgmtInfo, fHandle->curPagePos * PAGE_SIZE, SEEK_SET);
+    fseek(fHandle->mgmtInfo, (fHandle->curPagePos-1) * PAGE_SIZE, SEEK_SET);
     if(fwrite(memPage, PAGE_SIZE, 1, fHandle->mgmtInfo) != 1){
         printError("Write Error! Cannot write page to file!\n");
         return RC_WRITE_FAILED;
     }
     return RC_OK;
 }
-RC appendEmptyBlock (SM_FileHandle *fHandle){return 0;}
+
+RC appendEmptyBlock (SM_FileHandle *fHandle){
+    SM_PageHandle emptyPh;
+    if((emptyPh = (SM_PageHandle) malloc(PAGE_SIZE)) == NULL){
+        printError("Appending Error! Cannot allocate memory for one page!\n");
+        return RC_WRITE_FAILED;
+    }
+    strcpy(emptyPh, "");
+    for(int i = 0; i < PAGE_SIZE; ++i){
+        strcat(emptyPh, "\0");
+    }
+    if(fHandle->mgmtInfo == NULL) {
+        printError("Appending Error! File header not found!\n");
+        return RC_FILE_NOT_FOUND;
+    }
+    fseek(fHandle->mgmtInfo, fHandle->totalNumPages * PAGE_SIZE, SEEK_SET);
+    fprintf(fHandle->mgmtInfo, "%s", emptyPh);
+    fHandle->totalNumPages +=  1;
+    free(emptyPh);
+    return RC_OK;
+}
+
 RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){return 0;}
 //*****************************************************************************
